b8f40fbb6badeef864c7e33868cfb1bfa5936f66
Anggi <dwianggiana412@gmail.com>
2025-12-05 10:41:14 +0700

    update env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+app.use(express.json()); // middleware = 
 const PORT = 3300; 
 
 
@@ -31,6 +31,7 @@ let legacyProducts = [
   }
 ];
 
+// implementasi endpoint
 // get
 app.get('/products', (req, res) => {
     res.json(legacyProducts);
